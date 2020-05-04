bc5b763d28d2d0c3fb4be68304bbcd790172b18d
alanki789 <64784472+alanki789@users.noreply.github.com>
2020-05-04 18:13:50 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -7,20 +7,39 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test {
 
 	public static void main(String[] args) {
+		String FirstName = "Alankrita";
+		String Lastname = "Alankrita";
+		String EmailID = "alanki.alankrita7@gmail.com";
+		String Password = "qwerty123"
+		String URL = "http://elab-stg.labyrinthelab.com/create_learner_account.php";
+		String errorURL = "https://www.labyrinthelab.com/?error=00101"
+			
 		System.setProperty("webdriver.chrome.driver" , "C:\\Alankrita\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
-        driver.get("http://elab-stg.labyrinthelab.com/create_learner_account.php");
-	    driver.findElement(By.xpath("//input[@id='fname']")).sendKeys("Alankrita");
-	    driver.findElement(By.xpath("//input[@id='lname']")).sendKeys("Phillip");
-	    driver.findElement(By.xpath("//input[@id='email']")).sendKeys("alanki.alankrita7@gmail.com");
-	    driver.findElement(By.xpath("//input[@id='password']")).sendKeys("qwerty123");
-	    driver.findElement(By.xpath("//input[@id='cpassword']")).sendKeys("qwerty123");
-	    driver.findElement(By.xpath("//input[@type='checkbox']")).click();
-	    driver.findElement(By.xpath("//input[@id='createAccount']")).click();
-
-	    //driver.quit();
-		// TODO Auto-generated method stub
-
+        	driver.get("http://elab-stg.labyrinthelab.com/create_learner_account.php");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+	    	driver.findElement(By.xpath("//input[@id='fname']")).sendKeys(FirstName);
+	    	driver.findElement(By.xpath("//input[@id='lname']")).sendKeys(Lastname);
+	    	driver.findElement(By.xpath("//input[@id='email']")).sendKeys("alanki.alankrita7@gmail.com");
+	    	driver.findElement(By.xpath("//input[@id='password']")).sendKeys(Password);
+	    	driver.findElement(By.xpath("//input[@id='cpassword']")).sendKeys(Password);
+	    	driver.findElement(By.xpath("//input[@type='checkbox']")).click();
+	    	driver.findElement(By.id("createAccount")).click();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		String actualURL = driver.getCurrentURL();
+		
+		if (actualURL.equals(errorURL))
+		{
+			System.out.println("Invalid Username or Password Page");
+		}
+		else
+		{
+			System.out.println("Account Creation Failed");
+		}
+		
+	    driver.quit();
 	}
-
 }
